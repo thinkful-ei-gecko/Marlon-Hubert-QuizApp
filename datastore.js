aaf99b4fc2a9a0945f80e6f361bd336c59350b1c
Marlon Agno <mdj.agno@gmail.com>
2019-08-16 15:35:11 -0400

    added flexbox styling and datastore.js

---
+++ datastore.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const STORE = [{
+  question: 'Which character and tv show said this quote? "I am the one who knocks."',
+  answers: [
+    'Walter White from Breaking Bad',
+    'Kyle Chandler from Friday Night Lights',
+    'Marcia Brady from the Brady Bunch',
+    'Fox Mulder from The X-Files',
+  ],
+  correctAnswer: 'Walter White from Breaking Bad',
+
+}]
---
+++ index.js
@@ -0,0 +1,14 @@
+const STORE = [
+    {
+        question: 'Which character and tv show said this quote? "I am the one who knocks."',
+        answers: [
+            'Walter White from Breaking Bad',
+            'Kyle Chandler from Friday Night Lights',
+            'Marcia Brady from the Brady Bunch',
+            'Fox Mulder from The X-Files',
+            
+        ]
+
+        ]
+    }
+]
