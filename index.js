488fbe2f0c4c17336debcd81164f0de31c26c971
Marlon Agno <mdj.agno@gmail.com>
2019-08-16 16:48:05 -0400

    added to start-quiz button function

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 'use strict';
 
+let questionNum = 0;
+let currentScore = 0;
+
 function startQuiz() {
     $('.title-page').on('click', '.start-quiz', function(event) {
         event.preventDefault();
         $('.title-page').remove();
+        $('header').css('visibility','visible');
+      
     })
 };
 
-function generateQuestion() {
-    
+function generateQuestions() {
+    return `<div class ="questions">
+    <h2>Question 1</h2></div>`
 }
 
 function generateAnswers() {
@@ -27,4 +33,6 @@ function wrongAnswer() {
 
 };
 
+
 $(startQuiz);
+$(generateQuestion);
