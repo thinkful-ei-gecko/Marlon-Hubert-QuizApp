4692edd78edb4d11d6d57e6f675174dd6a70f202
Marlon Agno <mdj.agno@gmail.com>
2019-08-19 11:56:37 -0400

    added to datastore object

--- datastore.js
+++ datastore.js
@@ -10,6 +10,10 @@ const STORE = [
     'Fox Mulder from The X-Files'
   ],
   correctAnswer: 'Walter White from Breaking Bad',
+  answerImage: `<img class = 'answerImage' img src = 'https://imagesvc.timeincapp.com/v3/fan/
+  image?url=https%3A%2F%2F1428elm.com%2Ffiles%2F2017%2F04%2FX-Files-1.
+  jpg&c=sc&w=850&h=560'
+  img alt = 'Breaking Bad Title Image'>`
 },
 {
   question: 'Which show is this quote from? ..."to boldy go where no man has gone before."',
@@ -17,8 +21,53 @@ const STORE = [
     'Battlestar Galactica',
     'The Weakest Link',
     'Star Trek',
-    'Lost'
+    'Lost',
   ],
-  correctAnswer: 'Star Trek'
+  correctAnswer: 'Star Trek',
+  answerImage: `<img class = 'answerImage' img src = https://pmcvariety.files.wordpress.com/2014/11/star-trek.jpg?w=1000&h=563&crop=1>' 
+  img alt = 'Star Trek Title Image'>`
 }
-];
+],
+{
+  question: 'Which show is this quote from? ..."I sing in the shower. Sometimes I spend too much time volunteering. \n
+   Occasionally I\'ll hit somebody with my car."',
+  answers: [
+    'Ross Geller from Friends',
+    'Leslie Knope from Parks and Recreation',
+    'Charlie Kelly from It's Always Sunny in Philadelphia',
+    'Michael Scott from The Office'
+  ],
+  correctAnswer: 'Michael Scott from The Office',
+  answerImage: `<img class = 'answerImage' img src = 'https://images2.minutemediacdn.com/image/upload/c_
+  fill,w_912,h_516,f_auto,q_auto,g_auto/shape/cover/entertainment/5b2fefb9fae96f45b7000002.jpeg' img alt = 'The Office Image'>`
+}
+],
+{
+  question: 'Which show is this quote from? ..."Don\'t have a cow, man!."',
+  answers: [
+    'Joey Tribbiani from Friends',
+    'Sheldon Cooper from Big Bang Theory',
+    'Bart Simpson from The Simpsons',
+    'Dr. Gregory House from House'
+  ],
+  correctAnswer: 'Bart Simpson from The Simpsons',
+  answerImage: `<img class = 'answerImage' img src = https://pmctvline2.files.wordpress.com/2016/02/game-of-thrones-jon-snow.jpg?w=620> 
+  img alt = 'The Simpsons Title Image'>`
+}
+],
+{
+  question: 'Which show is this quote from? ..."I wound up shame-eating the whole pizza. I woke up all greasy and sweaty. \n
+  My sheets looked like what they wrap deli sandwiches in. Maybe I should join a gym. Do you go to a gym?"',
+  answers: [
+    'Michael Scott from the Office',
+    'Dr. John Dorian from Scrubs',
+    'Ted Mosby from How I Met Your Mother',
+    'Joey Tribbiani from Friends'
+  ],
+  correctAnswer: 'Ted Mosby from How I Met Your Mother',
+  answerImage: `<img class = 'answerImage' img src = https://miro.medium.com/max/1838/1*NwthyMSOHc8vi0wBLfcakA.jpeg
+  img alt = 'How I Met Your Mother Title Image'>`
+}
+],
+
+}
